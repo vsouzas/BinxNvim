940be11aa7ba19a4abdfffabbcb2fb5163e98d8b
Binx-Codes <Devchou2008@gmail.com>
2021-05-27 18:34:52 +0400

    fixed file explorer bug

--- lua/nvimtree/init.lua
+++ lua/nvimtree/init.lua
@@ -3,6 +3,7 @@ vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent ma
 vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = 1 -- Closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = 'startify' --empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_disable_window_picker = 1 -- 0 by default, will disable the window picker.
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
     vim.g.nvim_tree_bindings = {
       -- ["<CR>"] = ":YourVimFunction()<cr>",
